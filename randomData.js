99aefd52607480cf1354fd9513d05720c892c99e
wwm <wangwm@szhcf.com.cn>
2019-10-16 17:59:35 +0800

    ''

---
+++ randomData.js
@@ -0,0 +1,117 @@
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+var data = {
+    '周一': [{
+        'v': random(100, 600),
+        'color': '#EDB3FF'
+    }, {
+        'v': random(100, 600),
+        'color': '#D191E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#C4ADFF'
+    }, {
+        'v': random(100, 600),
+        'color': '#7F91E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#81B7FF'
+    }],
+    '周二': [{
+        'v': random(100, 600),
+        'color': '#EDB3FF'
+    }, {
+        'v': random(100, 600),
+        'color': '#D191E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#C4ADFF'
+    }, {
+        'v': random(100, 600),
+        'color': '#7F91E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#81B7FF'
+    }],
+    '周三': [{
+        'v': random(100, 600),
+        'color': '#EDB3FF'
+    }, {
+        'v': random(100, 600),
+        'color': '#D191E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#C4ADFF'
+    }, {
+        'v': random(100, 600),
+        'color': '#7F91E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#81B7FF'
+    }],
+    '周四': [{
+        'v': random(100, 600),
+        'color': '#EDB3FF'
+    }, {
+        'v': random(100, 600),
+        'color': '#D191E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#C4ADFF'
+    }, {
+        'v': random(100, 600),
+        'color': '#7F91E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#81B7FF'
+    }],
+    '周五': [{
+        'v': random(100, 600),
+        'color': '#EDB3FF'
+    }, {
+        'v': random(100, 600),
+        'color': '#D191E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#C4ADFF'
+    }, {
+        'v': random(100, 600),
+        'color': '#7F91E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#81B7FF'
+    }],
+    '周六': [{
+        'v': random(100, 600),
+        'color': '#EDB3FF'
+    }, {
+        'v': random(100, 600),
+        'color': '#D191E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#C4ADFF'
+    }, {
+        'v': random(100, 600),
+        'color': '#7F91E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#81B7FF'
+    }],
+    '周日': [{
+        'v': random(100, 600),
+        'color': '#EDB3FF'
+    }, {
+        'v': random(100, 600),
+        'color': '#D191E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#C4ADFF'
+    }, {
+        'v': random(100, 600),
+        'color': '#7F91E8'
+    }, {
+        'v': random(100, 600),
+        'color': '#81B7FF'
+    }]
+};
